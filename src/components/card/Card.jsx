97b107e0c12748e50916ab5f34c5d4ad112a9969
Watson kambo <wkwats@gmail.com>
2024-02-28 00:34:39 +0300

    Update Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -48,26 +48,6 @@ const Card = ({ key, item }) => {
               </span>
             </div>
           </div>
-
-          {/* 
-          <div className={styles.footer}>
-          <div className={styles.footerDiv}>
-            <div className={styles.footerIcon}>
-              <Image alt="heart" fill src="/heart.png" />
-            </div>
-            <span className={styles.footerVerticalLine}>|</span>
-            <span className={styles.authorName}>{kFormatter(item?.likes)}</span>
-          </div>
-          <div className={styles.footerDiv}>
-            <div className={styles.footerIcon}>
-              <Image alt="message" fill src="/message.png" />
-            </div>
-            <span className={styles.footerVerticalLine}>|</span>
-            <span className={styles.authorName}>
-              {kFormatter(item?.comments.length)}
-            </span>
-          </div>
-        </div> */}
         </div>
         <div className={styles.content}>
           <div className={styles.detail}>
@@ -81,7 +61,6 @@ const Card = ({ key, item }) => {
           <Link href={`/blog/${item.slug}`}>
             <h1 className={styles.posttitle}>{item.title}</h1>
           </Link>
-          {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
 
           <div className={styles.desc}>{parse(`${item?.desc}`)}</div>
 
@@ -92,84 +71,6 @@ const Card = ({ key, item }) => {
       </div>
     </div>
   );
-
-  // return (
-  //   <div className={styles.container} key={key}>
-  //     <div className={styles.imagecontainer}>
-  //       {item.img && (
-  //         <Image
-  //           src={item.img}
-  //           alt="main photo"
-  //           fill
-  //           className={styles.image}
-  //         />
-  //       )}
-  //     </div>
-  //     <div className={styles.textcontainer}>
-  //       <div className={styles.content}>
-  //         <div className={styles.user}>
-  //           <div className={styles.userInfo}>
-  //             <div className={styles.userImg}>
-  //               {item.user.image && (
-  //                 <Image
-  //                   src={item.user.image}
-  //                   fill
-  //                   alt="user image"
-  //                   style={{
-  //                     objectFit: "cover",
-  //                     borderRadius: "50%",
-  //                   }}
-  //                 />
-  //               )}
-  //             </div>
-  //             <div className={styles.details}>
-  //               {/* <Link href={`/authors/${item.user.name}`}>
-  //                 <span className={styles.authorName}>{item.user.name}</span>
-  //               </Link> */}
-  //               <span className={styles.authorName}>{item.user.name}</span>
-  //             </div>
-  //           </div>
-  //         </div>
-  //         <div className={styles.detail}>
-  //           <span className={styles.date}>
-  //             {moment(item.createdAt).fromNow()} -{" "}
-  //           </span>
-  //           <Link href={`/blog?cat=${item.catSlug}`}>
-  //             <span className={styles.category}>{item.category.title}</span>
-  //           </Link>
-  //         </div>
-
-  //         <Link href={`/blog/${item.slug}`}>
-  //           <span className={styles.userTitle}>{item.title}</span>
-  //         </Link>
-
-  //         <div className={styles.desc}>{parse(`${item?.desc}`)}</div>
-
-  //         <Link href={`/blog/${item.slug}`} className={styles.link}>
-  //           Read More
-  //         </Link>
-  //       </div>
-  //       <div className={styles.footer}>
-  //         <div className={styles.footerDiv}>
-  //           <div className={styles.footerIcon}>
-  //             <Image alt="heart" fill src="/heart.png" />
-  //           </div>
-  //           <span className={styles.footerVerticalLine}>|</span>
-  //           <span className={styles.authorName}>{kFormatter(item?.likes)}</span>
-  //         </div>
-  //         <div className={styles.footerDiv}>
-  //           <div className={styles.footerIcon}>
-  //             <Image alt="message" fill src="/message.png" />
-  //           </div>
-  //           <span className={styles.footerVerticalLine}>|</span>
-  //           <span className={styles.authorName}>
-  //             {kFormatter(item?.comments.length)}
-  //           </span>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default Card;
